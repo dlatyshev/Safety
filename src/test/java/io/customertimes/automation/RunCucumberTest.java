0ec164d2ab626317cdaaaf22c999362c16e39682
Dmytro Latyshev <dmytrolatyshev@administrators-MacBook-Pro.local>
2018-11-28 16:54:19 +0200

    Initial commit

---
+++ src/test/java/io/customertimes/automation/RunCucumberTest.java
@@ -0,0 +1,9 @@
+package io.customertimes.automation;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features = "src/test/resources/",
+plugin = {"pretty"}, glue = "steps")
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+}
---
+++ src/test/java/io/customertimes/automation/Stepdefs.java
@@ -0,0 +1,38 @@
+package io.customertimes.automation;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class Stepdefs {
+    @Given("user entered the website")
+    public void user_entered_the_website() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("user clicked the login button")
+    public void user_clicked_the_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("login page is opened")
+    public void login_page_is_opened() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("user entered valid credentials and press Enter")
+    public void user_entered_valid_credentials_and_press_Enter() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("org main page is opened")
+    public void org_main_page_is_opened() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
