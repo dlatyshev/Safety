9ed791537fac08db95052e0ca2c353d68c4267a2
dmytrolatyshev <d_latyshev@ukr.net>
2018-12-03 12:17:55 +0200

    New page object methods, new resources(chromedriver, geckodriver)

--- src/main/java/io/customertimes/automation/pages/Page.java
+++ src/main/java/io/customertimes/automation/pages/Page.java
@@ -1,15 +1,15 @@
 package io.customertimes.automation.pages;
 
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 
 public abstract class Page {
@@ -39,13 +39,14 @@ public abstract class Page {
     protected List<WebElement> findAll(By locator){
         return driver.findElements(locator);
     }
+
     protected void click(By locator){
-        waitForVisibilityOf(locator, 5);
+        waitForVisibilityOf(locator, 10);
         find(locator).click();
     }
 
     protected void type(String text, By locator){
-        waitForVisibilityOf(locator, 5);
+        waitForVisibilityOf(locator, 10);
         find(locator).sendKeys(text);
     }
 
@@ -67,6 +68,61 @@ public abstract class Page {
             attempts++;
 
         }
+    }
+
+    protected Alert switchToAlert() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.alertIsPresent());
+        return driver.switchTo().alert();
+    }
+
+    protected  void switchToWindowWithTitle(String expectedTitle){
+        String firstWindow = driver.getWindowHandle();
+
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String windowHandle : allWindows) {
+            if (!windowHandle.equals(firstWindow)) {
+                driver.switchTo().window(windowHandle);
+                if (getTitle().equals(expectedTitle)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    protected void switchToFrame(By frameLocator){
+        driver.switchTo().frame(find(frameLocator));
+    }
+
+    protected void pressKey(By locator, Keys key){
+        find(locator).sendKeys(key);
+    }
+
+    protected void pressKeyWithActions(Keys key){
+        log.info("Pressing " + key.name() + " using Actions class");
+        Actions action = new Actions(driver);
+        action.sendKeys(key).build().perform();
+    }
+
+    protected void performDragAndDrop(By from, By to){
+        Actions action = new Actions(driver);
+        action.dragAndDrop(find(from), find(to)).build().perform();
+        // Might not work with html5
+    }
+
+    public void scrollToBottom() {
+        log.info("Scrolling to the bottom of the page");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+    }
+
+    public String getCurrentPageTitle() {
+        return driver.getTitle();
+    }
+
+    public String getCurrentPageSource(){
+        return driver.getPageSource();
     }
 
     public String getCurrentUrl(){
--- src/main/java/io/customertimes/automation/utilities/BrowserDriverFactory.java
+++ src/main/java/io/customertimes/automation/utilities/BrowserDriverFactory.java
@@ -17,11 +17,14 @@ public class BrowserDriverFactory {
 
     public WebDriver createDriver() {
         log.info("Create driver: " + browser);
+
         switch (browser) {
             case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
                 driver.set(new ChromeDriver());
                 break;
             case "firefox":
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
                 driver.set(new FirefoxDriver());
                 break;
             default:
