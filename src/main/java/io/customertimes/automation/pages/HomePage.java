922b647661d79e23ef9b193cd9d1590e67eeba30
Roman Tyshchenko <>
2018-12-04 11:20:47 +0200

    Code clean up

--- src/main/java/io/customertimes/automation/pages/HomePage.java
+++ src/main/java/io/customertimes/automation/pages/HomePage.java
@@ -3,9 +3,6 @@ package io.customertimes.automation.pages;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends Page {
     private By appTitle = By.xpath("//span[@title='Safety - Intake']");
