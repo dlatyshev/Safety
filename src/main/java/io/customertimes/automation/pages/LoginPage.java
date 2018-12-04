57fe7ab688ddea2587742a83610ae901e1442d19
Roman Tyshchenko <>
2018-12-04 11:00:36 +0200

    commit

--- src/main/java/io/customertimes/automation/pages/LoginPage.java
+++ src/main/java/io/customertimes/automation/pages/LoginPage.java
@@ -3,9 +3,6 @@ package io.customertimes.automation.pages;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends Page {
 
@@ -39,6 +36,5 @@ public class LoginPage extends Page {
     @Override
     public String getCurrentUrl() {
         return super.getCurrentUrl();
-        // pull request 1 1:48
     }
 }
